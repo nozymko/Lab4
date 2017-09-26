a8d906d7da8eff07b508076d090aa1e5b97f0103
nozymko <nozymko@wirelessprv-10-194-19-207.near.illinois.edu>
2017-09-26 10:22:18 -0500

    Updated variable names

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -17,28 +17,29 @@ public class ReclamationProject {
      *
      * @param firstWord is a String
      * @param secondWord is a String
-     * @return the first char in first that also occurs in second
+     * @return the longest common substring
      */
     public static String containsSameThing(final String firstWord, final String secondWord) {
         String wordOne = firstWord;
         String wordTwo = secondWord;
+        //switches two words if word1 is longer than word2
         if (wordOne.length() > wordTwo.length()) {
-            String temp = wordOne; //switches two words
+            String temp = wordOne;
             wordOne = wordTwo;
             wordTwo = temp;
         }
-        String similar = "";
+        String longestSubstring = "";
         /*
          * For loop with i
          */
         for (int i = 0; i < wordOne.length(); i++) {
             for (int j = wordOne.length() - i; j > 0; j--) {
                 for (int k = 0; k < wordTwo.length() - j; k++) {
-                    if (wordOne.regionMatches(i, wordTwo, k, j) && j > similar.length()) {
-                        similar = wordOne.substring(i, i + j);
+                    if (wordOne.regionMatches(i, wordTwo, k, j) && j > longestSubstring.length()) {
+                        longestSubstring = wordOne.substring(i, i + j);
                     }
                 }
             }
-        } return similar;
+        } return longestSubstring;
     }
 }
